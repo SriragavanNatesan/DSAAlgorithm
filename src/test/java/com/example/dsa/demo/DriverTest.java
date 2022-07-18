c596a517e480b3f5354aab3369e3f5a147772f9b
Sriragavan N <ragavannsr@gmail.com>
2022-07-18 19:48:33 +0530

    Update

---
+++ src/test/java/com/example/dsa/demo/DriverTest.java
@@ -0,0 +1,13 @@
+package com.example.dsa.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverTest {
+    @Test
+    public  void Demotest(){
+        Driver.main(null);
+        assert(true);
+    }
+}
