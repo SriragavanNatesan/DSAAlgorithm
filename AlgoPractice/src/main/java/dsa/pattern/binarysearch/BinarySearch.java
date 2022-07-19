45cde1abc31314797d77f6493c9097f376df43b6
Sriragavan N <ragavannsr@gmail.com>
2022-07-19 18:56:08 +0530

    Binary search update for first and last occurence

--- AlgoPractice/src/main/java/dsa/pattern/binarysearch/BinarySearch.java
+++ AlgoPractice/src/main/java/dsa/pattern/binarysearch/BinarySearch.java
@@ -2,20 +2,77 @@ package dsa.pattern.binarysearch;
 
 public class BinarySearch {
     public static int binarySearch(int arr[], int target){
-        System.out.println("=====BinarySearch====");
+        System.out.println("=====binarySearch====");
         int start = 0;
         int end = arr.length - 1;
         while (start <= end){
-            int mid = start + (end - start) >> 1;
+            int mid = start + ((end - start) >> 1);
             if(arr[mid] == target){
                 return mid;
             }else if(arr[mid]  > target){
-                start = mid + 1;
-            }else {
                 end = mid - 1;
+            }else {
+                start = mid + 1;
             }
         }
 
         return -1;
+    }
+
+    public static int recursiveBinarySearch(int arr[], int target,int start,int end){
+
+//        System.out.println("=====recursiveBinarySearch====");
+        if(start <= end){
+            int mid = start + ((end - start) / 2 );
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid]  > target){
+                return recursiveBinarySearch(arr, target, start, mid -1);
+            }else {
+                return recursiveBinarySearch(arr, target, mid + 1, end);
+            }
+        }
+
+        return -1;
+    }
+
+    public static int firstOccurrence(int arr[], int target){
+        System.out.println("=====firstOccurence====");
+        int start = 0;
+        int end = arr.length - 1;
+        int result = -1;
+        while (start <= end){
+            int mid = start + ((end - start) >> 1);
+            if(arr[mid] == target){
+                result = mid;
+                end = mid -1;
+            }else if(arr[mid]  > target){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static int lastOccurrence(int arr[], int target){
+        System.out.println("=====lastOccurence====");
+        int start = 0;
+        int end = arr.length - 1;
+        int result = -1;
+        while (start <= end){
+            int mid = start + ((end - start) >> 1);
+            if(arr[mid] == target){
+                result = mid;
+                start = mid  + 1;
+            }else if(arr[mid]  > target){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+
+        return result;
     }
 }
--- AlgoPractice/src/test/java/dsa/pattern/binarysearch/BinarySearchTest.java
+++ AlgoPractice/src/test/java/dsa/pattern/binarysearch/BinarySearchTest.java
@@ -7,8 +7,50 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinarySearchTest {
 
     @Test
-    void binarySearch() {
+    void testbinarySearch() {
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertEquals(5, BinarySearch.binarySearch(arr, 5));
+        assertEquals(0, BinarySearch.binarySearch(arr, 0));
+        assertEquals(10, BinarySearch.binarySearch(arr, 10));
+        assertEquals(-1, BinarySearch.binarySearch(arr, 11));
+
+        assertEquals(9, BinarySearch.binarySearch(arr, 9));
+    }
+
+    @Test
+    void testrecursiveBinarySearch() {
+
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertEquals(5, BinarySearch.recursiveBinarySearch(arr, 5, 0, 10));
+        assertEquals(0, BinarySearch.recursiveBinarySearch(arr, 0, 0, 10));
+        assertEquals(10, BinarySearch.recursiveBinarySearch(arr, 10, 0, 10));
+        assertEquals(-1, BinarySearch.recursiveBinarySearch(arr, 11, 0, 10));
+        assertEquals(9, BinarySearch.recursiveBinarySearch(arr, 9, 0, 10));
+    }
+
+    @Test
+    void firstOccurence() {
+
+        int[] arr = {2, 4, 10, 10, 10, 18, 20, 20, 27};
+
+        assertEquals(-1, BinarySearch.firstOccurrence(arr, -1));
+        assertEquals(2, BinarySearch.firstOccurrence(arr, 10));
+        assertEquals(6, BinarySearch.firstOccurrence(arr, 20));
+        assertEquals(5, BinarySearch.firstOccurrence(arr, 18));
+
+        assertEquals(8, BinarySearch.firstOccurrence(arr, 27));
+    }
+
+    @Test
+    void lastOccurence() {
+
+        int[] arr = {2, 4, 10, 10, 10, 18, 20, 20, 27};
+
+        assertEquals(-1, BinarySearch.lastOccurrence(arr, -1));
+        assertEquals(4, BinarySearch.lastOccurrence(arr, 10));
+        assertEquals(7, BinarySearch.lastOccurrence(arr, 20));
+        assertEquals(5, BinarySearch.lastOccurrence(arr, 18));
+
+        assertEquals(8, BinarySearch.lastOccurrence(arr, 27));
     }
 }
